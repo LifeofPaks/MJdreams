63c6561bf7308b9a33f604483c3a62d30423c639
lifeofpaks <promise@alquimiavc.com>
2024-10-16 12:30:12 +0100

    created project components and files

---
+++ src/components/ExploreSection/Explore.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Explore"
+
+const Explore = () => {
+  return (
+    <div>Explore</div>
+  )
+}
+
+export default Explore
---
+++ src/components/ReviewSection/Review.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Review"
+
+const Review = () => {
+  return (
+    <div>Review</div>
+  )
+}
+
+export default Review
