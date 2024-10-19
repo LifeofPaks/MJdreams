eb9191230cc7b2d20344141d3d6b72289132e158
lifeofpaks <promise@alquimiavc.com>
2024-10-19 14:08:06 +0100

    updated services section

--- src/components/ServicesSection/Services.jsx
+++ src/components/ServicesSection/Services.jsx
@@ -32,13 +32,15 @@ const Services = () => {
           <div class="g1">
             <div className="img-wrapper">
               <div className="overlay"></div>
-              <h3 className="location">Sahara</h3>
+              <h3 className="location">Worldwide Ticket Reservation
+              </h3>
               <img className="m1" src={g1} alt="services-img" />
             </div>
 
             <div className="img-wrapper">
               <div className="overlay"></div>
-              <h3 className="location">Sahara</h3>
+              <h3 className="location">Holiday Packages
+              </h3>
               <img className="m2" src={g2} alt="services-img" />
             </div>
           </div>
@@ -46,19 +48,22 @@ const Services = () => {
             <div className="g2-1">
               <div className="img-wrapper">
                 <div className="overlay"></div>
-                <h3 className="location">Sahara</h3>
+                <h3 className="location">  ⁠⁠UAE Visa Service
+                </h3>
                 <img className="m3" src={g3} alt="services-img" />
               </div>
               <div className="g2-1-1">
                 <div className="img-wrapper">
                   <div className="overlay"></div>
-                  <h3 className="location">Sahara</h3>
+                  <h3 className="location">⁠⁠Limousine Service
+                  </h3>
                   <img className="m4" src={g4} alt="services-img" />
                 </div>
 
                 <div className="img-wrapper">
                   <div className="overlay"></div>
-                  <h3 className="location">Sahara</h3>
+                  <h3 className="location"> ⁠⁠Luxury Cruises
+                  </h3>
                   <img className="m5" src={g5} alt="services-img" />
                 </div>
               </div>
@@ -66,7 +71,8 @@ const Services = () => {
             <div className="g2-2">
               <div className="img-wrapper">
                 <div className="overlay"></div>
-                <h3 className="location">Sahara</h3>
+                <h3 className="location">Meet & Assst Service
+                </h3>
                 <img className="m6" src={g6} alt="services-img" />
               </div>
             </div>
@@ -74,13 +80,15 @@ const Services = () => {
           <div class="g3">
             <div className="img-wrapper">
               <div className="overlay"></div>
-              <h3 className="location">Sahara</h3>
+              <h3 className="location">Airport Transfers 
+              </h3>
               <img className="m7" src={g7} alt="services-img" />
             </div>
 
             <div className="img-wrapper">
               <div className="overlay"></div>
-              <h3 className="location">Sahara</h3>
+              <h3 className="location">Conferences Events & Conventions 
+              </h3>
               <img className="m8" src={g8} alt="services-img" />
             </div>
           </div>
@@ -88,31 +96,35 @@ const Services = () => {
         <div className="image-gallery-small">
           <div className="img-wrapper">
             <div className="overlay"></div>
-            <h3 className="location">Sahara</h3>
+            <h3 className="location">Worldwide Ticket Reservation</h3>
             <img className="m1" src={g1} alt="services-img" />
           </div>
 
           <div className="img-wrapper">
             <div className="overlay"></div>
-            <h3 className="location">Sahara</h3>
+            <h3 className="location">Holiday Packages
+            </h3>
             <img className="m2" src={g2} alt="services-img" />
           </div>
 
           <div className="img-wrapper">
             <div className="overlay"></div>
-            <h3 className="location">Sahara</h3>
+            <h3 className="location">  ⁠⁠UAE Visa Service
+            </h3>
             <img className="m3" src={g3} alt="services-img" />
           </div>
 
           <div className="img-wrapper">
             <div className="overlay"></div>
-            <h3 className="location">Sahara</h3>
+            <h3 className="location">Worldwide Hotel Reservation 
+            </h3>
             <img className="m4" src={g4} alt="services-img" />
           </div>
 
           <div className="img-wrapper">
             <div className="overlay"></div>
-            <h3 className="location">Sahara</h3>
+            <h3 className="location">⁠⁠Limousine Service
+            </h3>
             <img className="m5" src={g5} alt="services-img" />
           </div>
         </div>
